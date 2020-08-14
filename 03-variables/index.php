0d9e3b2fe263de4125aff5c1865e07ee085c7120
Jery Ramirez <rafa85jogo@gmail.com>
2020-08-13 22:41:22 -0600

    tipos de datos

--- 03-variables/index.php
+++ 03-variables/index.php
@@ -1,8 +1,10 @@
 <?php
 //variable es un espacio en memoria
 $mi_primera_variable = 'Hola mundo en php';
+$numero = 2;
 
 echo '<h1>'.$mi_primera_variable.'</h1>';
-
+echo '<h1>'.$numero.'</h1>';
+echo '<h1>'.$numero.'</h1>';
 
 ?>
---
+++ 04-tipos/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * TIPOS DE DATOS
+ * ENTERO
+ * FLOAT
+ * STRING
+ * BOOLEANOS
+ * NULL
+ * ARRAY
+ * OBJETOS 
+ */
+
+ $numero = "100.5";
+$decimal = 15.5;
+$texto = "texto";
+
+
+$verdadero = false;
+    echo $verdadero;
+    echo "<br>";
+
+ echo gettype($verdadero)//funcion que me permite obtener el tipo de dato
+
+?>
