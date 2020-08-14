80de7b752dd7f775b8a32d21233d546789577c70
Jery Ramirez <rafa85jogo@gmail.com>
2020-08-13 22:06:07 -0600

    imprimir por pantalla y comentario en codigo

---
+++ 02-imprimir/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Imprimir por pantalla - Master en php</title>
+    </head>
+    <body>
+        <h1>Master en php -  <?php echo "jery.es";?> </h1>
+       
+ <?php
+        echo "<h3>Listado de videojuegos:</h3>";
+        echo "<ul>"
+            ."<li>GTA</li>"
+            ."<li>Fifa</li>"
+            ."<li>Mario Bros</li>"
+            ."</ul>";
+        echo '<p> Esta es toda'.' - '.'lista de juegos</p>';
+    ?>        
+
+    
+    </body>
+</html>
+
