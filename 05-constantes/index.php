0c204f29f139723ddab414401718cf0e47fb13f8
Jery Ramirez <rafa85jogo@gmail.com>
2020-08-20 16:30:30 -0600

    constantes

---
+++ 05-constantes/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//constantes
+//es un contenedor de información que no puede variar
+
+define('nombre','Jery');
+define('web', 'jeryweb');
+
+//imprimir constante
+echo '<h1>'.nombre.'</h1>';
+echo '<h1>'.web.'<h1>';
+
+?>
