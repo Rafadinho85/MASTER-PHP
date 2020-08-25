0cf60e7d113f5767e06413a3095c4ca78c154a66
Jery Ramirez <rafa85jogo@gmail.com>
2020-08-24 20:40:35 -0600

    operadores arimeticos

--- 05-constantes/index.php
+++ 05-constantes/index.php
@@ -10,4 +10,8 @@ define('web', 'jeryweb');
 echo '<h1>'.nombre.'</h1>';
 echo '<h1>'.web.'<h1>';
 
+//constantes predefinadas
+echo PHP_OS.'<br>';
+echo PHP_VERSION.'<br>';
+echo PHP_EXTENSION_DIR.'<br>'
 ?>
---
+++ 06-operadores/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+Operadores aritmeticos
+ *  */
+
+$numero1 = 55;
+$numero2 = 33;
+
+echo 'Suma: '.($numero1+$numero2).'<br>';
+echo 'Resta: '.($numero1-$numero2).'<br>';
+echo 'Multi: '.($numero1*$numero2).'<br>';
+echo 'Division: '.($numero1/$numero2).'<br>';
+echo 'Modulo: '.($numero1%$numero2).'<br>';
+
+
+
