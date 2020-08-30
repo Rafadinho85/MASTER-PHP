740c2d059d6cd26c6ec031096d3c919cbef69e37
Jery Ramirez <rafa85jogo@gmail.com>
2020-08-29 22:45:21 -0600

    operadores

--- 06-operadores/index.php
+++ 06-operadores/index.php
@@ -13,5 +13,29 @@ echo 'Multi: '.($numero1*$numero2).'<br>';
 echo 'Division: '.($numero1/$numero2).'<br>';
 echo 'Modulo: '.($numero1%$numero2).'<br>';
 
+//OPERADORES INCREMENTO Y DECREMENTO
 
+$year = 2019;
+//decremento
+// $year = $year - 1
+$year--;
+//incremento
+//$year = $year + 1
+$year++;
 
+//pre-decremento
+//$year = 1 - $year
+--$year;
+
+//pre-incremento
+//$year = 1 + $year
+
+++$year;
+
+echo "<h1>$year<h1/>";
+
+//operadores de asignación
+
+$edad = 50;
+echo $edad.'<br>';
+echo ($edad+5);
