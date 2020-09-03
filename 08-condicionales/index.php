bfd1189d1651111ad15717d44f3f484e7654414f
Jery Ramirez <rafa85jogo@gmail.com>
2020-09-02 20:29:31 -0600

    condicionales

---
+++ 08-condicionales/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+CONDICIONALES
+OPERADORES DE COMPARACION
+
+
+*/
+
+$color = "BLUE";
+if ($color == "red") {
+    echo "EL COLOR ES RED";
+}else {
+    echo "EL COLOR ES AMARILLO";
+}
+
