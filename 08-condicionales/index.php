5af0016a76538e79e0126f2e68cbd47a0fe8ae69
Jery Ramirez <rafa85jogo@gmail.com>
2020-09-02 20:37:54 -0600

    elseif

--- 08-condicionales/index.php
+++ 08-condicionales/index.php
@@ -3,7 +3,15 @@
 /* 
 CONDICIONALES
 OPERADORES DE COMPARACION
-
+== igual
+=== identico compara hasta el tipo de dato
+!= diferente
+<> diferente
+!== no identico
+< mayor que 
+> menor que
+<= menor o igual que
+>= mayor o igual que
 
 */
 
@@ -13,4 +21,20 @@ if ($color == "red") {
 }else {
     echo "EL COLOR ES AMARILLO";
 }
-
+echo "<br>";
+//ejemplo2 con elseif
+$dia = 1;
+if ($dia == 1) {
+    echo "lunes";    
+}elseif ($dia == 2) {
+    echo "martes";
+}elseif ($dia == 3) {
+    # code...
+    echo "miercoles";
+}elseif ($dia == 4) {
+    # code...
+    echo "jueves";
+}elseif ($dia == 5) {
+    # code...
+    echo "viernes";
+}
