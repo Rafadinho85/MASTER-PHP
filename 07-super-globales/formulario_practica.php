14c468ed8cce85e5a37cd4f7ef96849c1cda9f37
Jery Ramirez <rafa85jogo@gmail.com>
2020-09-02 20:18:48 -0600

    practicando

---
+++ 07-super-globales/formulario_practica.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FORMULARIO DE PRACTICA</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Formulario practica</h1>
+        <form method="POST" action="recibir_practica.php">
+            <p>
+            <label>Nombre</label>
+            <input type="text" name="nombre">    
+            </p>
+            <p>
+                <label>Apellido</label>
+                <input type="text" name="apellido">
+            </p>
+            <input type="submit">
+            
+        </form>
+    </body>
+</html>
+
+
